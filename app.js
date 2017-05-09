f33545a5fbf4dfd550282302da212e0c349c1074
Mike Gozzo <mike@smooch.io>
2017-05-08 22:15:44 -0400

    vary the dicks

--- app.js
+++ app.js
@@ -128,8 +128,8 @@ app.post('/smooch', function(req, res) {
 
                 console.log(faces[i]);
 
-                dicks[0].resize(faces[i].faceRectangle.width*1.25, faces[i].faceRectangle.height*1.8);
-                img.composite(dicks[0], faces[i].faceRectangle.left-faces[i].faceRectangle.width/3, faces[i].faceRectangle.top-faces[i].faceRectangle.height/3);
+                dicks[i % 7].resize(faces[i].faceRectangle.width*1.25, faces[i].faceRectangle.height*1.8);
+                img.composite(dicks[i % 7], faces[i].faceRectangle.left-faces[i].faceRectangle.width/3, faces[i].faceRectangle.top-faces[i].faceRectangle.height/3);
               }
             }
 
